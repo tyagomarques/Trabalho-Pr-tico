e58992eee65a917e51f5bae28a69d5aa3dd3455e
Hilarious1999 <hilarious1999@users.noreply.github.com>
2018-12-07 16:50:57 +0000

    Revert "test"
    This reverts commit 3507e60d15efc34fae8a3debaf91b6207762a286.

--- src/trabalhopratico/Admin.java
+++ src/trabalhopratico/Admin.java
@@ -53,64 +53,26 @@ public class Admin extends Pessoa{
             }
     }
       
-     //----------------------------------
-    public void menuDeCriaConta()
-    {
-        System.out.println("\t 1 - Adicionar Pessoa; \n\t 2 - Remover Pessoa;\n");
-        int escolha2 = Ler.umInt();
-        switch(escolha2){       // Sub-menu 1
-            case 1: // Inserir Pessoa
-                System.out.println("Nome da Pessoa : ");
-                String nome = Ler.umaString();
-
-              break;
-            case 2: // Remover Pessoa
-
-
-            break;
-        }
-    }
-    
-            
-            
-    //----------------------------------
     public void menu(){
         int escolha = 0;
-        System.out.println("1 – Gestão Pessoas;\n" + "2 – Gestão Contas;\n" + "3 – Gestão Tarefas;\n" + "4 – Remov."  + "5.Sair\n");
-  
+        System.out.println("1 – Gestão Pessoas;\n" + "2 – Gestão Contas;\n" + "3 – Gestão Tarefas;\n" + "4 – Sair.\n");
         escolha = Ler.umInt();
-        while(escolha != 5){ // Menu Inicial
+        while(escolha != 4){       // Menu Inicial
             switch(escolha){
                 case 1: 
                     
                     break;
                 case 2:
-                    System.out.println("1 - Adicionar Conta a Pagar;\n" + "2 - Remover Conta a Pagar;\n");
-                    int escolha3 = Ler.umInt();
-                    switch(escolha3){       // Sub-menu 1
-                        case 1: // Inserir conta
-                            
-                            System.out.println("Valor da conta : ");
-                            int valor = Ler.umInt();
-                            
-                          break;
-                        case 2: // Remover conta
-                            
-                            
-                        break;
-                    }
+                    
                     break;
                         
                 case 5:
                     break;
-                    
-                default: 
-                        System.out.println("Introduziu um número inválido!");
                 
             }
             System.out.println("Opção : ");
             escolha = Ler.umInt();
         }
-
     }
 }
+//--------------------------
