9f820b26578e51c54b26f0941c15e5fb579316a9
tyagomarques <43614367+tyagomarques@users.noreply.github.com>
2018-12-10 21:51:34 +0000

    Finalmente consegui enviar
    Reparei que alguém começou a fazer por mim mas tenho coisas a acrescentar 1º Menu interativo 2º inicio do admin id passsword 3º Correção de erro e acrescentar notas

--- src/trabalhopratico/TrabalhoPratico.java
+++ src/trabalhopratico/TrabalhoPratico.java
@@ -8,24 +8,31 @@ package trabalhopratico;
 import java.util.ArrayList;
 import myinputs.Ler;
 
+import javax.swing.*;
+
 /**
  *
- * @author Diogo Pinheiro
+ * @author tyagomarques
  */
 public class TrabalhoPratico {
-
+    private static String password,valor,nome;
+    private static int escolha;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        ArrayList<Pessoa> membros = new ArrayList<Pessoa> ();
-        
-        for (int i = 0; i < Pessoa.getNum(); i++)
-        {
-            System.out.println(membros.get(i).getNota());
+        menu();
+        ArrayList<Pessoa> membros = new ArrayList<>();
+        try {
+            for (int i = 0; i < Pessoa.getNum(); i++) {
+                System.out.println(membros.get(i).getNota()); /* Lembrem-se que este aqui não devia estar cá mas sim no menu*/
+                                                                /*E devem criar uma função que leia um ficheiro!!!*/
+            }
         }
-        
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Vazio\n");
+        }
         //-------------------------------------------------------//
         int sair = 0;
         int escolha;
@@ -35,33 +42,63 @@ public class TrabalhoPratico {
             System.out.println("2 - Consultar Contas a pagar");
             System.out.println("3 - Consultar Tarefas");
             System.out.println("4 - Sair");
-            
+
             escolha = Ler.umInt();
-            
+
             switch(escolha)
             {
-                case '1' : ;
-                    
-                case '2' : ;
-                
-                case '3' : ;
-                
-                case '4' :
-                                sair = 1;
+                case '1' : break;
+
+                case '2' : break;
+
+                case '3' : break;
+
+                case '4' : sair = 1;
+                            break;
                                 // gravar no ficheiro//
             }
-            
+
         }
         while(sair == 0);
-        
     }
-    
-    public void Menu(){
+
+    private static void menu(){
         /* 1- Mostrar Notificações
            2- Consultar Contas a pagar/Histórico de contas
            3- Consultar Tarefas
            4- Menu Admin (necessita de verificação de password)
         */
-        
+
+        JOptionPane.showMessageDialog(null,"Seja bem vindo à Gerência de Tarefas","Gerência de Tarefas",JOptionPane.INFORMATION_MESSAGE);
+        do {
+            escolha = 0;
+
+            valor = JOptionPane.showInputDialog(null,
+                    "0- Sair\n"+
+                             "1- Mostrar Notificações\n" +
+                             " 2- Consultar Contas a pagar/Histórico de contas\n" +
+                             " 3- Consultar Tarefas\n" +
+                             " 4- Menu Admin (necessita de verificação de password)\n" +
+                             "Introduza um inteiro:","Gerência de Tarefas",JOptionPane.INFORMATION_MESSAGE);
+            if (valor != null) {
+                escolha = Integer.parseInt(valor);
+                switch (escolha){
+                    case 1:
+                            break;
+                    case 2:
+                            break;
+                    case 3:
+                            break;
+                    case 4: nome = JOptionPane.showInputDialog(null,"Nome:","Admin",JOptionPane.PLAIN_MESSAGE);
+                            password = JOptionPane.showInputDialog(null,"Password: ","Admin",JOptionPane.PLAIN_MESSAGE);
+                            break;
+                }
+
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Adeus!!","Gerência de Tarefas",JOptionPane.INFORMATION_MESSAGE);
+            }
+        } while(escolha==0);
+        System.exit(0);
     }
 }
