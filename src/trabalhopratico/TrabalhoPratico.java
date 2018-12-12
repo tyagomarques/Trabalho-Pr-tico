630cba252efcde9b40f7ba985905d093514346fe
Hilarious1999 <hilarious1999@users.noreply.github.com>
2018-12-12 17:10:51 +0000

    Password
    Adicionei a password

--- src/trabalhopratico/TrabalhoPratico.java
+++ src/trabalhopratico/TrabalhoPratico.java
@@ -17,17 +17,56 @@ public class TrabalhoPratico {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        ArrayList<Pessoa> membros = new ArrayList<Pessoa> ();
+    public static void main(String[] args) throws Exception {
         
-        for (int i = 0; i < Pessoa.getNum(); i++)
+        
+        ArrayList<Pessoa> membros = new ArrayList<Pessoa> ();
+        Pessoa a = new Pessoa("fabio","putas");
+        membros.add(a);
+        String alma;
+        String password;
+        int ind = -1;
+        
+        do{                           // PROCURA A PESSOA NA LISTA E DEVOLVE O SEU
+            System.out.print("Nome :");   // ID, REPETE-SE ENQUANTO O NOME NAO EXISTIR
+            alma = Ler.umaString();                    // NA LISTA
+            for(int i = 0; i < membros.size(); i++)
+            {
+                if(membros.get(i).getNome().equals(alma))
+                {
+                ind = i;
+                break;
+                }
+            }
+        }while(ind == -1);
+        
+        int tentativas = 0;
+        System.out.println("");        // PEDE A PASSWORD ENQUANTO NÃO A ACERTA
+        do{
+            System.out.print("Password :");
+            password = Ler.umaString();
+            if(membros.get(ind).getPass().equals(password))
+            {
+               break; 
+            }
+            tentativas++;
+        }while (tentativas < 5);
+        
+        if(tentativas == 5)
         {
+            throw new Exception("Excedeste o número de tentativas");
+        }
+        
+        
+        
+        for (int i = 0; i < Pessoa.getNum(); i++)     // APRESENTA AS NOTAS DOS 
+        {                                               // RESIDENTES
             System.out.println(membros.get(i).getNota());
         }
         
+        
         //-------------------------------------------------------//
-        int sair = 0;
+        
         int escolha;
         do
         {
@@ -40,28 +79,19 @@ public class TrabalhoPratico {
             
             switch(escolha)
             {
-                case '1' : ;
+                case '1' : break;
                     
-                case '2' : ;
+                case '2' : break;
                 
-                case '3' : ;
+                case '3' : break;
                 
                 case '4' :
-                                sair = 1;
+                                
                                 // gravar no ficheiro//
+                                break;
             }
             
         }
-        while(sair == 0);
-        
-    }
-    
-    public void Menu(){
-        /* 1- Mostrar Notificações
-           2- Consultar Contas a pagar/Histórico de contas
-           3- Consultar Tarefas
-           4- Menu Admin (necessita de verificação de password)
-        */
-        
+        while(escolha != 4);   
     }
 }
